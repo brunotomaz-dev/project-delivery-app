85bd2e0b3b3ec9ec32ac8cc7952350df4a355be9
brunotomaz-dev <tomaz.bruno@gmail.com>
2023-06-09 19:39:20 -0300

    criado dockerfile e docker compose, além de ajustes no package.json para criar banco ao inicializar

--- front-end/src/helpers/instance.js
+++ front-end/src/helpers/instance.js
@@ -1,7 +1,15 @@
 import axios from 'axios';
+import "dotenv/config";
+
+const HOST = (
+  process.env.REACT_APP_API_HOST 
+  || `${process.env.REACT_APP_HOSTNAME}:${process.env.REACT_APP_BACKEND_PORT}`
+  || 'localhost:3001'
+  );
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: `${PROTOCOL}://${HOST}`,
 });
 
 export const requestData = async (endpoint, options = {}) => {
